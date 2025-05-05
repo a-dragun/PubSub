53a0e7a5ca5be65360b8e63d79658d1c1691bffd
a-dragun <antevdragun@gmail.com>
2025-05-05 21:15:42 +0200

    Add simple websocket communication inside rooms

--- server.js
+++ server.js
@@ -9,11 +9,30 @@ const authMiddleware = require("./middleware/authMiddleware");
 const userRoutes = require("./routes/user");
 const roomRoutes = require("./routes/rooms");
 const methodOverride = require("method-override");
+const http = require('http');
+const socketIO = require('socket.io');
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+const io = socketIO(server);
+app.set('io', io);
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('chatMessage', (msg) => {
+    io.emit('chatMessage', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,6 +43,11 @@ app.use(session({
   saveUninitialized: false 
 }));
 app.use(methodOverride("_method"));
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
 
 app.set("view engine", "ejs");
 
@@ -39,4 +63,4 @@ app.use("/questions", authMiddleware.requireAuth, questionRoutes);
 app.use("/user", authMiddleware.requireAuth, userRoutes);
 app.use("/rooms", authMiddleware.requireAuth, roomRoutes);
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
